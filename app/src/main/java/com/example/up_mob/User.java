20a52d63f4d5e0f97b49de0a14d275da1fd83604
ДрейИА <dreyvania999@gmail.com>
2023-03-03 17:14:05 +0300

    Теперь работает с картинкой

--- app/src/main/java/com/example/up_mob/User.java
+++ app/src/main/java/com/example/up_mob/User.java
@@ -1,6 +1,8 @@
 package com.example.up_mob;
 
-public class User {
+import java.io.Serializable;
+
+public class User implements Serializable {
     private int id;
     private String email;
     private String nickName;
